7512d321c43f3a18edde7b61806ae17ccf1b7b01
EsthelaSalazar96 <56266161+EsthelaSalazar96@users.noreply.github.com>
2019-10-11 12:37:57 -0500

    Add files via upload

---
+++ Canaliza/index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Prueba</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+
+
+  <div class="Encabezado">
+   <img src="img/logo2019.jpg" class="x" alt="2019">
+  </div>
+
+	<div class="login-box">
+      <img src="img/logo2.png" class="avatar" alt="Avatar Image">
+      <h1>Iniciar sesión</h1>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Usuario</label>
+        <input type="text" placeholder="Nombre de usuario">
+        <!-- PASSWORD INPUT -->
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Contraseña">
+        <input type="submit" value="Log In">
+      
+      </form>
+    </div>
+</body>
+</html>
